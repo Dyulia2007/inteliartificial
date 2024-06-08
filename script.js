337ecd8bbdcd7476cabcddc65336f70dcf97291f
Dyulia2007 <145811700+Dyulia2007@users.noreply.github.com>
2024-06-07 22:24:27 -0300

    s

--- script.js
+++ script.js
@@ -3,3 +3,8 @@ const caixaPerguntas = document.querySelector(".caixa-perguntas");
 const caixaAlternativas = document.querySelector(".caixa-alternativas");
 const caixaResultado = document.querySelector(".caixa-resultado");
 const textoResultado = document.querySelector(".texto-resultado");
+
+const perguntas = [
+    "Pergunta 1",
+    "Pergunta 2"
+];
